40c0b739a489fa71a76c95175ecd29248f521722
Cezary Sanecki <csanecki@gmail.com>
2019-05-29 15:20:56 +0200

    deleted unnecessary fragment

--- src/com/spoj/pl/easy/E978.java
+++ src/com/spoj/pl/easy/E978.java
@@ -33,14 +33,4 @@ public class E978 {
 		
 		scanner.close();
 	}
-	
-	static void serveHeap(String[] array) {
-		for(int i = 0; i < array.length / 2; i++) {
-			String temp = array[i];
-			array[i] = array[array.length - 1 - i];
-			array[array.length - 1 - i] = temp;
-		}
-		
-		System.out.println(String.join(" ", array));
-	}
 }
