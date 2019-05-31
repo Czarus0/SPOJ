1b7b30470fffed7b717a4588c3165db708ffff49
Cezary Sanecki <csanecki@gmail.com>
2019-05-31 09:40:54 +0200

    refactored task 1055

--- src/com/spoj/pl/easy/E1055.java
+++ src/com/spoj/pl/easy/E1055.java
@@ -3,10 +3,6 @@ package com.spoj.pl.easy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class E1055 {
 	public static void main(String[] args) throws IOException {
@@ -16,23 +12,19 @@ public class E1055 {
 		for(int i = 1; i <= numberOfTests; i++) {
 			String[] passedData = reader.readLine().split(" ");
 						
-			List<Integer> evens = new LinkedList<Integer>();
-			List<Integer> odds = new LinkedList<Integer>();
+			int numberOfNumbers = Integer.valueOf(passedData[0]);
 			
-			for(int j = 1; j < passedData.length; j++) {
-				Integer number = Integer.valueOf(passedData[j]);
-				
+			String[] orderedNumbers = new String[numberOfNumbers];
+						
+			for(int j = 1; j <= numberOfNumbers; j++) {				
 				if(j % 2 == 0) {
-					evens.add(number);
+					orderedNumbers[(j - 1) / 2] = passedData[j];
 				} else {
-					odds.add(number);
+					orderedNumbers[numberOfNumbers / 2 + (j - 1) / 2] = passedData[j];
 				}
 			}
-			
-			evens.addAll(odds);
-			
-			int[] result = evens.stream().mapToInt(Integer::intValue).toArray();
-			String sortedString = Arrays.stream(result).mapToObj(String::valueOf).collect(Collectors.joining(" "));
+
+			String sortedString = String.join(" ", orderedNumbers);
 			
 			System.out.println(sortedString);
 		}
